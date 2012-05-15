230b92cf995d7e58c9d9d45a151fd7fb8ac5cb19
Steve Shaddick <steve@steveshaddick.com>
2012-05-15 00:33:22 -0400

    - portfolio begin

--- www/models/Main.php
+++ www/models/Main.php
@@ -31,6 +31,42 @@ class Main {
 			//assume today, request is coming from the ajax call
 			$noWork = $this->mySQL->getSingleRow("SELECT * FROM NoWork ORDER BY dateShown DESC");
 			return $noWork;
+
+	public function getPortfolioThumbs() {
+		
+		return $this->mySQL->sendQuery("SELECT workId, title, previewType, medium, thumb FROM Work WHERE section LIKE '%portfolio%' ORDER BY dateReleased DESC");
+	}
+	
+	public function getPortfolioNoWork() {
+			
+		$noWork = array(
+				'type' => 'quote',
+				'description' => "Web developer working in the advertising industry; primarily using HTML(5), CSS(3), javascript, AS3, and PHP.<br /><br />
+				I enjoy the craft of developing clean, well-built, dependable digital solutions. Most of the time this means a website, but I also build back-end tools and scripts for design. My job is to enable and enhance ideas as projects move from conception into code.<br /><br />
+				This site is my online portfolio of professional projects. For personal / art projects, see <a href=\"http://steveshaddick.com\" title=\"steveshaddick.com\">here</a>."
+				);
+				
+			$this->mySQL->insertRows($noWork, 'NoWork', true);
+
+			return $noWork;
+	}
+	
+	public function getNoWork() {
+		
+		function getMetaData($url){
+			// get meta tags
+			$meta=get_meta_tags($url);
+			// store page
+			$page=file_get_contents($url);
+			// find where the title CONTENT begins
+			$titleStart=stripos($page,'<title>')+7;
+			// find how long the title is
+			$titleLength=stripos($page,'</title>')-$titleStart;
+			// extract title from $page
+			$meta['title']=substr($page,$titleStart,$titleLength);
+			// return array of data
+			return $meta;
+
 		}
 		
 		if (isset($_GET['nowork'])) {
