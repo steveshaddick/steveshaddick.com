41c43f47e332a2953d1965fec5e6a2065050a54b
Steve Shaddick <steve@steveshaddick.com>
2013-06-03 20:43:43 -0400

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('steveshaddick.com.jquery.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    
+    compass: {
+      d: {
+        options: {
+          sassDir: 'src/scss/',
+          cssDir: 'www/css',
+          imagesDir: 'www/images',
+          outputStyle: 'compressed',
+          environment: 'production'
+        }
+      }
+    },
+
+    uglify: {
+        dev: {
+          options: {
+            preserveComments: 'some',
+            compress: false,
+            beautify: true
+          },
+          files: {
+            'www/js/plugins.min.js' : [
+              "src/js/swfaddress/swfaddress.js",
+              "src/js/jquery/jquery.mousewheel.min.js",
+              "src/js/jquery/jquery.fullscreen.js",
+              "src/js/jquery/jquery.cookie.min.js",
+              "src/js/lightbox/lightbox.js",
+              "src/js/simplevideo/SimpleVideo.js"
+            ],
+            'www/js/plugins-phone.min.js' : [
+              "src/js/swfaddress/swfaddress.js",
+              "src/js/jquery/jquery.cookie.min.js",
+              "src/js/iscroll/iscroll.js",
+              "src/js/lightbox/lightbox.js",
+              "src/js/simplevideo/SimpleVideo.js"
+            ],
+            'www/js/Main.min.js': [
+              'src/js/Main.js'
+            ]
+          }
+        },
+        prod: {
+          options: {
+            preserveComments: 'some'
+          },
+           files: {
+            'www/js/plugins.min.js' : [
+              "src/js/swfaddress/swfaddress.js",
+              "src/js/jquery/jquery.mousewheel.min.js",
+              "src/js/jquery/jquery.fullscreen.js",
+              "src/js/jquery/jquery.cookie.min.js",
+              "src/js/lightbox/lightbox.js",
+              "src/js/simplevideo/SimpleVideo.js"
+            ],
+            'www/js/plugins-phone.min.js' : [
+              "src/js/swfaddress/swfaddress.js",
+              "src/js/jquery/jquery.cookie.min.js",
+              "src/js/iscroll/iscroll.js",
+              "src/js/lightbox/lightbox.js",
+              "src/js/simplevideo/SimpleVideo.js"
+            ],
+            'www/js/Main.min.js': [
+              'src/js/Main.js'
+            ]
+          }
+        }
+      },
+    watch: {
+      styles: {
+        files: 'src/scss/*.scss',
+        tasks: ['compass'],
+        options: {
+          interrupt: true
+        }
+      },
+      scripts: {
+        files: 'src/js/**/*.js',
+        tasks: ['uglify:dev'],
+        options: {
+          interrupt: true
+        }
+      }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Default task.
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('prod', ['compass', 'uglify:prod']);
+
+};
---
+++ README.md
@@ -0,0 +1,30 @@
+# steveshaddick.com
+
+The online cornucopia of Steve Shaddick.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/steveshaddick/steveshaddick.com/master/dist/steveshaddick.com.min.js
+[max]: https://raw.github.com/steveshaddick/steveshaddick.com/master/dist/steveshaddick.com.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/steveshaddick.com.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
